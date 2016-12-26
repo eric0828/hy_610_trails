1329e50eee139c961c15069ddf0b0d10edf529cc
Eric.Li <eric0828@126.com>
2016-12-27 00:00:43 +0800

    add  new extension:cuppytrail.

---
+++ cuppytrail/src/de/hybris/platform/cuppytrail/setup/CuppytrailSystemSetup.java
@@ -0,0 +1,44 @@
+/*
+ * [y] hybris Platform
+ * 
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of SAP 
+ * Hybris ("Confidential Information"). You shall not disclose such 
+ * Confidential Information and shall use it only in accordance with the 
+ * terms of the license agreement you entered into with SAP Hybris.
+ */
+package de.hybris.platform.cuppytrail.setup;
+
+import static de.hybris.platform.cuppytrail.constants.CuppytrailConstants.PLATFORM_LOGO_CODE;
+
+import de.hybris.platform.core.initialization.SystemSetup;
+
+import java.io.InputStream;
+
+import de.hybris.platform.cuppytrail.constants.CuppytrailConstants;
+import de.hybris.platform.cuppytrail.service.CuppytrailService;
+
+
+@SystemSetup(extension = CuppytrailConstants.EXTENSIONNAME)
+public class CuppytrailSystemSetup
+{
+	private final CuppytrailService cuppytrailService;
+
+	public CuppytrailSystemSetup(final CuppytrailService cuppytrailService)
+	{
+		this.cuppytrailService = cuppytrailService;
+	}
+
+	@SystemSetup(process = SystemSetup.Process.INIT, type = SystemSetup.Type.ESSENTIAL)
+	public void createEssentialData()
+	{
+		cuppytrailService.createLogo(PLATFORM_LOGO_CODE);
+	}
+
+	private InputStream getImageStream()
+	{
+		return CuppytrailSystemSetup.class.getResourceAsStream("/cuppytrail/sap-hybris-platform.png");
+	}
+}
