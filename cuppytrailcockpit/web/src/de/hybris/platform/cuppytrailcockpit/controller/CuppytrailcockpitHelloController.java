d4e794a1608e372d6119e24ff082f20f94663a68
Eric.Li <libin539@tasly.com>
2017-01-10 18:06:19 +0800

    12.Cockpit Basics：create the cuppytrailcockpit extension.

---
+++ cuppytrailcockpit/web/src/de/hybris/platform/cuppytrailcockpit/controller/CuppytrailcockpitHelloController.java
@@ -0,0 +1,37 @@
+/*
+ * [y] hybris Platform
+ * 
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ * 
+ * This software is the confidential and proprietary information of SAP 
+ * Hybris ("Confidential Information"). You shall not disclose such 
+ * Confidential Information and shall use it only in accordance with the 
+ * terms of the license agreement you entered into with SAP Hybris.
+ */
+package de.hybris.platform.cuppytrailcockpit.controller;
+
+import static de.hybris.platform.cuppytrailcockpit.constants.CuppytrailcockpitConstants.PLATFORM_LOGO_CODE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.hybris.platform.cuppytrailcockpit.service.CuppytrailcockpitService;
+
+
+@Controller
+public class CuppytrailcockpitHelloController
+{
+	@Autowired
+	private CuppytrailcockpitService cuppytrailcockpitService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(final ModelMap model)
+	{
+		model.addAttribute("logoUrl", cuppytrailcockpitService.getHybrisLogoUrl(PLATFORM_LOGO_CODE));
+		return "welcome";
+	}
+}
